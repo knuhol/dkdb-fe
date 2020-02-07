817104d8c5fdc5e5d0eea25d7e674322b941f4d3
Knut Holm <knut.holm28@gmail.com>
2020-02-07 15:52:42 +0100

    feat: improvment of SEO

--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from 'react';
 import { Container } from 'react-bootstrap';
+import { Helmet } from 'react-helmet';
 
 import Loader from './Loader';
 import Menu from './Menu';
@@ -8,11 +9,26 @@ import Footer from './Footer';
 type PageProps = {
   id?: string;
   loading?: boolean;
+  title?: string;
+  description?: string;
   children?: ReactNode;
 };
 
-const Page = ({ id, loading, children }: PageProps) => (
+const Page = ({ id, loading, title, description, children }: PageProps) => (
   <>
+    <Helmet>
+      <title>{title} | DKDB</title>
+      <meta name="description" content={description} />
+      <meta property="og:title" content={`${title} | DKDB`} />
+      <meta property="og:description" content={description} />
+      <meta property="og:type" content="website" />
+      <meta property="og:locale" content="cs_CZ" />
+      <meta property="og:site_name" content="DKDB" />
+      <meta property="og:image" content={`${process.env.PUBLIC_URL}/images/dkdb-og.png`} />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+      <meta property="og:image:type" content="image/png" />
+    </Helmet>
     <header>
       <Menu />
     </header>
@@ -26,6 +42,8 @@ const Page = ({ id, loading, children }: PageProps) => (
 Page.defaultProps = {
   id: undefined,
   loading: false,
+  title: 'Česká databáze duhových knih',
+  description: 'Všechny LGBT knihy v češtině přehledně na jednom místě.',
 };
 
 export default Page;
